63f7eba47fa97de108f99855100c7dcbe44442f9
John Kristan <jkpogz09@gmail.com>
2023-12-08 14:27:58 +0800

    put the oauth cred in the .env

--- app/server/src/config/google.strategy.ts
+++ app/server/src/config/google.strategy.ts
@@ -9,9 +9,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google'){
 
     constructor(){
         super({
-            clientID: '770093344597-trhjaj9s9ds12hnp4jkhv6s0eou6nhci.apps.googleusercontent.com',
-            clientSecret: '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',
-            callbackURL: 'http://localhost:4000/auth/google/redirect',
+            clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
+            callbackURL:process.env.GOOGLE_AUTH_REDIRECT_URI,
             scope: ['email', 'profile'],
         })
     }
